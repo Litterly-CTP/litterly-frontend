dee69ff11a95a9c7c98305fc15111c9ea00b6156
Joyce <joycalation@gmail.com>
2019-12-04 14:03:10 -0500

    Add landing and profile files. Need to reroute

---
+++ views/index.js
@@ -0,0 +1,28 @@
+// Sticky fixed navbar. On scroll the bg color should turn opaque 
+
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $("#mainNavbar");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    });
+});
+
+function onSuccess(googleUser) {
+    console.log(‘Logged in as: ’ + googleUser.getBasicProfile().getName());
+
+}
+function onFailure(error) {
+    console.log(error);
+}
+
+function renderButton() {
+    gapi.signin2.render(‘my-signin2’, {
+        ‘scope’: ‘profile email’,
+        ‘width’: 240,
+        ‘height’: 50,
+        ‘longtitle’: true,
+        ‘theme’: ‘dark’,
+        ‘onsuccess’: onSuccess,
+        ‘onfailure’: onFailure
+    });
+}
